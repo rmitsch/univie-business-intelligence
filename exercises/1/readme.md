a5444a74d499e8c29f86ce13707c22f9c1789b06
Raphael Mitsch <laarem33@hotmail.com>
2019-04-03 12:00:13 +0200

    Proceeded with data integration.

--- exercises/1/readme.md
+++ exercises/1/readme.md
@@ -5,20 +5,56 @@ The original SQL code was translated to a SQLite dialect using https://www.jooq.
 
 ### Todos
 * Read up on MXML model.
-  * what's a process case? universal MXML term or model-specific?
-  * process case <-> patient?
-  * activity <-> krankenhausaufenthalt, medikation, ...?
-  * parameter <-> attribute?
 * Transform database data -> integration model.
 * Transform spreadsheet data -> integration model.
+
+
+
+
 
 ## Tasks
 
 **1. Create a list of problems and assumptions when/for doing the integration. (2 points)**  
-* dsdsd
+* Patients in spreadsheet and database are treated as different entities, since their cases don't obviously overlap and 
+hence we can't reasonably assume they are the same persons.
+* Data consistency w.r.t. to usage of parameters is guaranteed at runtime / when new data is entered.
+* Medications are considered complete instantaneously.
 
 **2. Integrate the data into the given data integration model from Figure 1. (4 points)**  
-* dsdsd
+
+Table and spreadsheet mapping:
+* Patient <-> Processcase
+* Leistung, KrankenhausaufenthaltLeistung, Medication <-> ProcessCaseActivity + parameter tables
+* "Operation", "Krankenhausaufenthalt", "Medikation", various column header in spreadsheet <-> Activity
+-> use name instead of just "Operation" so we can't duplicate params? applies to all params though. we could presume 
+additional constraints preventing multiple params with the same name for one Processcase (except those which explicitly 
+can occur multple times, such as "depends on" or "derived from").
+
+Processcase:
+* externalidentifier -> patient ID
+
+ProcesscaseParameters (assuming Processcase <-> Krankenhausaufenthalt):
+* Processcase ID
+* Patient first name
+* Patient last name
+* Patient year of birth
+* Patient month of birth
+* Patient day of birth 
+* Patient gender
+
+
+ProcessCaseActivityParameters (assuming ProcesscaseActivity <-> KrankenhausAufenthaltsLeistung + Medikation):
+* Medication name
+* Medication dosage
+* Name of operation
+* MRT diagnosis
+* CT diagnosis
+* Localisation
+* LDH marker
+
+TODO how to reflect depencency for diagnoses? param for "depends on other activity"?
+lowest common denominator for normalizing data (e. g. exzision)
+matching patients?
 
 **3. Export the data from the data integration model into MXML and submit the resulting MXML file.
 One option to do so is using the SQLXML standard. (3 points)**  
--- exercises/1/run.py
+++ exercises/1/run.py
@@ -1,3 +1,5 @@
+import re
+
 import pandas as pd
 import argparse
 
@@ -7,5 +9,88 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     df = pd.read_excel(args.input, sheet_name=0)
+
+    # Map fixed values for inserts.
+    values = {
+        "Activity": [
+            "operation", "hospital_stay", "medication", "examination", "diagnosis"
+        ],
+        "ProcesscaseParameters": [
+            ("patient_first_name", 1),
+            ("patient_last_name", 1),
+            ("patient_year_of_birth", 0),
+            ("patient_month_of_birth", 0),
+            ("patient_day_of_birth", 0),
+            ("patient_gender", 1)
+        ],
+        "ProcesscaseActivityParameters": [
+            ("medication_name", 1),
+            ("medication_dosage", 0),
+            ("operation_name", 1),
+            ("diagnosis_name", 1),
+            ("diagosis_result", 1),
+            # Note: depends_on_pca_id reflects dependency of this PCA on one other PCA.
+            ("depends_on_pca_id", 0)
+        ]
+    }
+
+    sql = ""
+
+    # todo Format insert of PossibleValues. What's suposed to go in here?
+    # sql += """
+    #     insert into PossibleValue
+    # """
+
+    # 1. Activities.
+    sql += "insert into Activity (name) values {activities}".format(
+        activities="('" + "'), ('".join(values["Activity"]) + "')"
+    )
+
+    # 2. Parameters.
+    vals = values["ProcesscaseParameters"]
+    vals.extend(values["ProcesscaseActivityParameters"])
+    for val in values["ProcesscaseParameters"]:
+        sql += "\ninsert into Parameter (name, type) values ('{name}', {type})".format(name=val[0], type=val[1])
+
+    # 3. Processcases.
+    sql += "\ninsert into Processcase (externalidentifier) select id from Patient"
+    sql += "\ninsert into Processcase (externalidentifier) values {vals}".format(
+        vals="('" + "'), ('".join(df.Patient.values.tolist()) + "')"
+    )
+
+    # 4. ProcesscaseActivity.
+    # todo What's time precision?
+
+    # 4. 1. Medications from DB.
+    sql += "\n" + """
+        insert into ProcesscaseActivity (processcaseId, activityId, starttime, stoptime, timeprecision) 
+            select 
+                p.id, a.id, m.ausgabe, m.ausgabe, 0 
+            from 
+                Medikation m 
+            inner join Activity a on 
+                a.name = 'medication'
+            inner join Processcase p on
+                cast(p.externalidentifier as int) = m.patientId
+    """.replace("\n", " ").strip()
+
+    # 4. 2. Medications from spreadsheet.
+    for patient_id in df[df.Therapie.notnull()].Patient.values:
+        sql += "\n" + """
+            insert into ProcesscaseActivity (processcaseId, activityId, starttime, stoptime, timeprecision) 
+                select 
+                    p.id, a.id, m.ausgabe, m.ausgabe, 0 
+                from 
+                    Medikation m 
+                inner join Activity a on 
+                    a.name = 'medication'
+                inner join Processcase p on
+                    p.externalidentifier = '{patient_id}'
+        """.format(patient_id=patient_id).replace("\n", " ").strip()
+
+    # 4. 3. Operations
+
     with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.width', None):
-        print(df)
+        print(df[df.Therapie.notnull()])
+
+    print(re.sub(r"( +)", " ", sql))
