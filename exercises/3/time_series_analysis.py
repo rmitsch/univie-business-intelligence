d653af3701ee00fb9c9ebffca45a2ef439e3c626
Raphael Mitsch <laarem33@hotmail.com>
2019-06-01 17:34:27 +0200

    Added 1.4.

--- exercises/3/time_series_analysis.py
+++ exercises/3/time_series_analysis.py
@@ -1,38 +1,15 @@
-import prophet
 import pandas as pd
 import os
 from matplotlib import pyplot as plt
 from scipy.stats import binom_test
-
-
-def compute_salescount_per_week(transactions: pd.DataFrame, allowed_product_ids: set) -> pd.DataFrame:
-    """
-    Computes sales counts per week for specified set of products.
-    :param transactions:
-    :param allowed_product_ids:
-    :return: Dataframe with sales counts per week, separated by product ID.
-    """
-
-    return transactions[
-        transactions.UPC.isin(allowed_product_ids)
-    ][["UPC", "WEEK_END_DATE", "UNITS"]].groupby(["UPC", "WEEK_END_DATE"]).sum()
-
-
-def compute_salescount_per_week_plot_df(salescount_by_week: pd.DataFrame, products: pd.DataFrame) -> pd.DataFrame:
-    """
-    Auxiliary method to prepare dataframe holding weekly sales count data for plotting.
-    :param salescount_by_week:
-    :param products:
-    :return: Dataframe that can be used for plotting.
-    """
-
-    return pd.pivot_table(
-        salescount_by_week.join(products)[["extended_description", "UNITS"]].reset_index().drop(
-            columns=["UPC"], errors="ignore"
-        ),
-        index="WEEK_END_DATE",
-        columns=["extended_description"]
-    ).reset_index()
+from pprint import pprint
+from fbprophet import Prophet
+import numpy as np
+import itertools
+from tqdm import tqdm
+import utils
+from multiprocessing.pool import ThreadPool, Pool
+import psutil
 
 
 if __name__ == '__main__':
@@ -69,12 +46,15 @@ if __name__ == '__main__':
         # 1. 2.
         #################################
 
-        salescount_by_week = compute_salescount_per_week(transactions, set(top5_prods_by_sales.index.values))
+        salescount_by_week = utils.compute_salescount_per_week(transactions, set(top5_prods_by_sales.index.values))
         products["extended_description"] = products["DESCRIPTION"] + " (" + products.index.astype(str) + ")"
-        salescount_by_week_plot_df = compute_salescount_per_week_plot_df(salescount_by_week, products)
-        salescount_by_week_plot_df.plot(x="WEEK_END_DATE", y="UNITS", title="Sales count per week")
-        plt.grid()
-        plt.show()
+        salescount_by_week_plot_df = utils.compute_salescount_per_week_plot_df(salescount_by_week, products)
+
+        # salescount_by_week_plot_df.plot(x="WEEK_END_DATE", y="UNITS", title="Sales count per week")
+        # plt.plot(salescount_by_week_plot_df.WEEK_END_DATE, salescount_by_week_plot_df.UNITS)
+        # plt.title("Sales count per week")
+        # plt.grid()
+        # plt.show()
 
         #################################
         # 1. 3.
@@ -96,22 +76,156 @@ if __name__ == '__main__':
         #     )
 
         # Plot occurence of promotions - one plot per promotion and top 5 product.
-        for column in ("FEATURE", "DISPLAY", "TPR_ONLY"):
-            for ix, row in top5_prods_by_sales.iterrows():
-                extended_description = row.DESCRIPTION + " (" + str(ix) + ")"
-                salescount_by_week_plot = salescount_by_week.join(
-                    products
-                )[["extended_description", "UNITS"]].reset_index().drop(
-                    columns=["UPC"], errors="ignore"
-                ).plot(
-                    x="WEEK_END_DATE",
-                    y="UNITS",
-                    title="Sales count per week with " + column + " promotions for \n " + extended_description
+        # for column in ("FEATURE", "DISPLAY", "TPR_ONLY"):
+        #     for ix, row in top5_prods_by_sales.iterrows():
+        #         extended_description = row.DESCRIPTION + " (" + str(ix) + ")"
+        #         salescount_by_week_plot = salescount_by_week.join(
+        #             products
+        #         )[["extended_description", "UNITS"]].reset_index().drop(
+        #             columns=["UPC"], errors="ignore"
+        #         ).plot(
+        #             x="WEEK_END_DATE",
+        #             y="UNITS",
+        #             title="Sales count per week with " + column + " promotions for \n " + extended_description
+        #         )
+        #
+        #         for dt in transactions[(transactions.UPC == ix) & (transactions[column] == 1)].WEEK_END_DATE.unique():
+        #             salescount_by_week_plot.axvline(dt, color='r', linestyle='--', lw=1)
+        #
+        #         plt.grid()
+        #         plt.show()
+
+        #################################
+        # 1. 4.
+        #################################
+
+        # W/o any tuning:
+        # for ix in top5_prods_by_sales.index.values:
+        #     # Split in train and test set..
+        #     base_df = salescount_by_week.loc[ix].reset_index().rename(columns={"WEEK_END_DATE": "ds", "UNITS": "y"})
+        #     split_date = base_df.max()["ds"] - pd.Timedelta(4, unit='w')
+        #     train_set = base_df[base_df.ds <= split_date]
+        #     test_set = base_df[base_df.ds > split_date]
+        #
+        #     proph = Prophet()
+        #     proph.fit(train_set)
+        #     future = proph.make_future_dataframe(periods=4, freq="W")
+        #     forecast = proph.predict(future)
+        #     # proph.plot(forecast)
+        #     # proph.plot_components(forecast)
+        #     # plt.show()
+        #
+        #     # MSE
+        #     mse = np.mean(np.power(test_set.y - forecast.yhat.tail(4), 2))
+        #     print(mse)
+
+        base_dfs = {
+            upc: salescount_by_week.loc[upc].reset_index().rename(columns={"WEEK_END_DATE": "ds", "UNITS": "y"})
+            for upc in top5_prods_by_sales.index.values
+        }
+
+        if not os.path.isfile("ts_gridsearch_results.pkl"):
+            # Hyperparameter tuning:
+            hyperparameters = [
+                # yearly_seasonality
+                [True, False],
+                # custom monthly_seasonality
+                [True, False],
+                # weekly_seasonality
+                [True, False],
+                # daily_seasonality
+                [True, False],
+                # seasonality_mode
+                ["additive", "multiplicative"],
+                # n_changepoints
+                [5, 15, 25, 35, 50],
+            ]
+            prophet_configs = [
+                (upc, hp_config, base_dfs[upc])
+                for upc in top5_prods_by_sales.index.values
+                for hp_config in [
+                    config for config in itertools.product(*hyperparameters)
+                ]
+            ]
+
+            # Hyperparameter grid search.
+            with Pool(psutil.cpu_count(logical=True)) as thread_pool:
+                config_results = list(
+                    tqdm(
+                        thread_pool.imap(utils.run_prophet_configuration_wrapper, prophet_configs),
+                        total=len(prophet_configs)
+                    )
                 )
 
-                for dt in transactions[(transactions.UPC == ix) & (transactions[column] == 1)].WEEK_END_DATE.unique():
-                    salescount_by_week_plot.axvline(dt, color='r', linestyle='--', lw=1)
+            ts_gridsearch_results = pd.DataFrame(config_results)
+            ts_gridsearch_results.to_pickle("ts_gridsearch_results.pkl")
+        else:
+            ts_gridsearch_results = pd.read_pickle("ts_gridsearch_results.pkl")
 
-                plt.grid()
-                plt.show()
+        # for upc in top5_prods_by_sales.index.values:
+        #     print(ts_gridsearch_results[ts_gridsearch_results.UPC == upc].sort_values("MSE", ascending=True).head(5))
+        #     print("-" * 32)
+
+        # yes, hyperparameter settings do make a difference. it's not drastic, but noticeable.
+
+        best_configs_by_upc = {
+            upc: ts_gridsearch_results[ts_gridsearch_results.UPC == upc].sort_values("MSE", ascending=True).iloc[0][[
+                "yearly_seasonality",
+                "monthly_seasonality",
+                "weekly_seasonality",
+                "daily_seasonality",
+                "seasonality_mode",
+                "n_changepoints",
+                "MSE"
+            ]]
+            for upc in top5_prods_by_sales.index.values
+        }
+
+        # Use holidays.
+        for upc in best_configs_by_upc:
+            config = best_configs_by_upc[upc]
+            result_without_holidays = utils.evaluate_prophet_configuration(
+                upc,
+                [
+                    config.yearly_seasonality,
+                    config.monthly_seasonality,
+                    config.weekly_seasonality,
+                    config.daily_seasonality,
+                    config.seasonality_mode,
+                    config.n_changepoints
+                ],
+                base_dfs[upc]
+            )["MSE"]
+            result_with_holidays = utils.evaluate_prophet_configuration(
+                upc,
+                [
+                    config.yearly_seasonality,
+                    config.monthly_seasonality,
+                    config.weekly_seasonality,
+                    config.daily_seasonality,
+                    config.seasonality_mode,
+                    config.n_changepoints
+                ],
+                base_dfs[upc],
+                holidays=pd.concat([
+                    pd.DataFrame({
+                        'holiday': promotion_type,
+                        'ds': transactions[
+                            (transactions.UPC == upc) & (transactions[promotion_type] == 1)
+                        ].WEEK_END_DATE.unique(),
+                        'lower_window': 0,
+                        'upper_window': 0,
+                    })
+                    for promotion_type in ("FEATURE", "DISPLAY", "TPR_ONLY")
+                ])
+            )["MSE"]
+
+            relative_MSE_delta = (result_without_holidays - result_with_holidays) / result_without_holidays
+            print(f"Delta for UPC #{upc}: {relative_MSE_delta}")
+            print(f"Without holidays: MSE = {result_without_holidays}, with holidays: MSE = {result_with_holidays}")
+            print("-" * 30)
+
+
+
+
 
---
+++ exercises/3/utils.py
@@ -0,0 +1,120 @@
+import os
+import pandas as pd
+from fbprophet import Prophet
+import numpy as np
+import io
+import sys
+
+
+class suppress_stdout_stderr(object):
+    """
+    A context manager for doing a "deep suppression" of stdout and stderr in
+    Python, i.e. will suppress all print, even if the print originates in a
+    compiled C/Fortran sub-function.
+       This will not suppress raised exceptions, since exceptions are printed
+    to stderr just before a script exits, and after the context manager has
+    exited (at least, I think that is why it lets exceptions through).
+    Source: https://stackoverflow.com/questions/11130156/suppress-stdout-stderr-print-from-python-functions.
+    """
+
+    def __init__(self):
+        # Open a pair of null files
+        self.null_fds = [os.open(os.devnull, os.O_RDWR) for x in range(2)]
+        # Save the actual stdout (1) and stderr (2) file descriptors.
+        self.save_fds = (os.dup(1), os.dup(2))
+
+    def __enter__(self):
+        # Assign the null pointers to stdout and stderr.
+        os.dup2(self.null_fds[0], 1)
+        os.dup2(self.null_fds[1], 2)
+
+    def __exit__(self, *_):
+        # Re-assign the real stdout/stderr back to (1) and (2)
+        os.dup2(self.save_fds[0], 1)
+        os.dup2(self.save_fds[1], 2)
+        # Close the null files
+        os.close(self.null_fds[0])
+        os.close(self.null_fds[1])
+
+
+def compute_salescount_per_week(transactions: pd.DataFrame, allowed_product_ids: set) -> pd.DataFrame:
+    """
+    Computes sales counts per week for specified set of products.
+    :param transactions:
+    :param allowed_product_ids:
+    :return: Dataframe with sales counts per week, separated by product ID.
+    """
+
+    return transactions[
+        transactions.UPC.isin(allowed_product_ids)
+    ][["UPC", "WEEK_END_DATE", "UNITS"]].groupby(["UPC", "WEEK_END_DATE"]).sum()
+
+
+def compute_salescount_per_week_plot_df(salescount_by_week: pd.DataFrame, products: pd.DataFrame) -> pd.DataFrame:
+    """
+    Auxiliary method to prepare dataframe holding weekly sales count data for plotting.
+    :param salescount_by_week:
+    :param products:
+    :return: Dataframe that can be used for plotting.
+    """
+
+    return pd.pivot_table(
+        salescount_by_week.join(products)[["extended_description", "UNITS"]].reset_index().drop(
+            columns=["UPC"], errors="ignore"
+        ),
+        index="WEEK_END_DATE",
+        columns=["extended_description"]
+    ).reset_index()
+
+
+def run_prophet_configuration_wrapper(args) -> dict:
+    """
+    Wrapper for multiprocessing runs of run_prophet_configuration().
+    :param args:
+    :return:
+    """
+    return evaluate_prophet_configuration(*args)
+
+
+def evaluate_prophet_configuration(
+        upc: int, config: list, base_df: pd.DataFrame, holidays: pd.DataFrame = None
+) -> dict:
+    """
+    Runs and evaluates specified Prophet configuration.
+    :param upc:
+    :param config:
+    :param base_df:
+    :param holidays:
+    :return:
+    """
+    # Split in train and test set.
+    split_date = base_df.max()["ds"] - pd.Timedelta(4, unit='w')
+    train_set = base_df[base_df.ds <= split_date]
+    test_set = base_df[base_df.ds > split_date]
+
+    with suppress_stdout_stderr():
+        proph = Prophet(
+            yearly_seasonality=config[0],
+            weekly_seasonality=config[2],
+            daily_seasonality=config[3],
+            seasonality_mode=config[4],
+            n_changepoints=config[5],
+            holidays=holidays
+        )
+        if config[1]:
+            proph.add_seasonality(name='monthly', period=30.5, fourier_order=3)
+        proph.fit(train_set)
+        future = proph.make_future_dataframe(periods=4, freq="W")
+
+    # Return configuration + MSE result.
+    return {
+        "UPC": upc,
+        "yearly_seasonality": config[0],
+        "monthly_seasonality": config[1],
+        "weekly_seasonality": config[2],
+        "daily_seasonality": config[3],
+        "seasonality_mode": config[4],
+        "n_changepoints": config[5],
+        "MSE": np.mean(np.power(test_set.y - proph.predict(future).yhat.tail(4), 2))
+    }
+
